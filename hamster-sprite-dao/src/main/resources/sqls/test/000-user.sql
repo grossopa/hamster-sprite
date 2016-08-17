8d42c4653213488f26a767ea849e0bcc24e48090
Jack Yin <grossopaforever@gmail.com>
2016-08-17 23:09:24 +0800

    WIP

---
+++ hamster-sprite-dao/src/main/resources/sqls/test/000-user.sql
@@ -0,0 +1,2 @@
+-- table user rows count 1, Date 8/17/16 10:58 PM
+INSERT INTO user (id, user_id, password, salt, _created_by, _created_on, _updated_by, _updated_on, _status) VALUES (1, 'grossopa', '7k8bR+AYGTwyDbQTyAYdYBol5PF3PPBUQo1LxqhpLCM=', 'AAAAAAAAAAAA', 'SYSTEM', '2016-07-08 15:00:00', 'SYSTEM', '2016-07-08 15:00:00', 'ACTIVE');
