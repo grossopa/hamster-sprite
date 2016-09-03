59c218552004e43129da0f104c445e46b19dba39
Jack Yin <grossopaforever@gmail.com>
2016-09-03 11:39:57 +0800

    WIP

--- hamster-sprite-portal/src/main/java/org/hamster/sprite/portal/Application.java
+++ hamster-sprite-portal/src/main/java/org/hamster/sprite/portal/Application.java
@@ -5,9 +5,8 @@ package org.hamster.sprite.portal;
 
 import javax.sql.DataSource;
 
-import org.hamster.core.api.environment.builder.DefaultEnvironmentBuilder;
-import org.hamster.core.api.environment.builder.EnvironmentBuilder;
-import org.springframework.boot.SpringApplication;
+import org.hamster.core.web.spring.boot.AbstractApplication;
+import org.hamster.sprite.portal.interceptor.PageInterceptor;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -26,27 +25,26 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAutoConfiguration
 @EntityScan("org.hamster.sprite.dao.entity")
 @EnableJpaRepositories("org.hamster.sprite.dao")
-@ComponentScan({"org.hamster.sprite"})
-public class Application {
+@ComponentScan({ "org.hamster.sprite" })
+public class Application extends AbstractApplication {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-        System.setProperty(DefaultEnvironmentBuilder.ENVIRONMENT_VARIABLE, environmentBuilder().build().toString());
-        SpringApplication.run(Application.class, args);
+        AbstractApplication.create(Application.class).run(args);
     }
-    
+
     @Bean
     @Primary
-    @ConfigurationProperties(prefix="datasource.primary")
+    @ConfigurationProperties(prefix = "datasource.primary")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
     }
-    
-    public static EnvironmentBuilder environmentBuilder() {
-        return new DefaultEnvironmentBuilder();
+
+    @Bean
+    public PageInterceptor pageInterceptor() {
+        return new PageInterceptor();
     }
-    
 
 }
