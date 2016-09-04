dbea9d76d43186d0e85fb27164d546079416f59f
Jack Yin <grossopaforever@gmail.com>
2016-09-04 23:21:05 +0800

    WIP

--- hamster-sprite-portal/src/main/java/org/hamster/sprite/portal/Application.java
+++ hamster-sprite-portal/src/main/java/org/hamster/sprite/portal/Application.java
@@ -3,6 +3,11 @@
  */
 package org.hamster.sprite.portal;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import org.hamster.core.web.spring.boot.AbstractApplication;
@@ -23,6 +28,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
  * main application entrance
@@ -55,8 +64,18 @@ public class Application extends AbstractApplication {
     @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
     protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
+        public AuthenticationSuccessHandler httpAuthenticationSuccessHandler() {
+            return new DefaultAuthenticationSuccessHandler();
+        }
+
+        public AuthenticationFailureHandler httpAuthenticationFailureHandler() {
+            return new DefaultAUthenticationFailureHandler();
+        }
+
         @Override
         protected void configure(HttpSecurity http) throws Exception {
+            
+            
             //@formatter:off
             http
                 .authorizeRequests()
@@ -67,19 +86,33 @@ public class Application extends AbstractApplication {
                 .formLogin()
                     .usernameParameter(SecurityConsts.PARAM_USERNAME)
                     .passwordParameter(SecurityConsts.PARAM_PASSWORD)
-                    .successHandler()
-                    
-                
-                    
+                    .successHandler(httpAuthenticationSuccessHandler())
+                    .failureHandler(httpAuthenticationFailureHandler())
+                    .permitAll();
             //@formatter:on
         }
 
         @Override
         public void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN", "USER").and().withUser("user").password("user").roles("USER");
         }
 
-        public static class DefaultAuthenticationSuccessHandler extends AuthenticationSuccessHandler {
+        public static class DefaultAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+                    throws IOException, ServletException {
+
+            }
+
+        }
+
+        public static class DefaultAUthenticationFailureHandler implements AuthenticationFailureHandler {
+
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+                    throws IOException, ServletException {
+                
+            }
 
         }
 
