7a0c901bb13cf33f470200101dd871e71ca0e70d
Jack Yin <grossopaforever@gmail.com>
2017-03-23 22:37:49 +0800

    WIP

--- hamster-sprite-dao/src/test/java/com/hamster/sprite/dao/test/base/AbstractAppDaoSpringTest.java
+++ hamster-sprite-dao/src/test/java/com/hamster/sprite/dao/test/base/AbstractAppDaoSpringTest.java
@@ -3,39 +3,21 @@
  */
 package com.hamster.sprite.dao.test.base;
 
+import org.hamster.core.test.repository.AbstractDaoSpringTest;
 import org.hamster.sprite.dao.SpriteDaoConfiguration;
 import org.hamster.sprite.dao.entity.UserEntity;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author <a href="mailto:grossopaforever@gmail.com">Jack Yin</a>
  * @version 1.0
  */
-@RunWith(SpringRunner.class)
-@DataJpaTest
 @SpringBootTest(classes = SpriteDaoConfiguration.class)
-public abstract class AbstractDaoSpringTest {
+public abstract class AbstractAppDaoSpringTest extends AbstractDaoSpringTest {
 
-    @Autowired
-    protected TestEntityManager entityManager;
-
-    private boolean initialized;
-
-    @Before
-    public void before() {
-        if (!initialized) {
-            initCommonData();
-            initialized = true;
-        }
-    }
-
-    protected void initCommonData() {
+    @Override
+    protected void initCommonData(TestEntityManager entityManager) {
         // 123456
         entityManager.persistAndFlush(
                 UserEntity.newInstance("TEST1", "$2a$10$PX.WvBHYExVN/V9XH48.ruaBcl67zEIEV.F/AIGviee5JyG9khymO"));
--- hamster-sprite-dao/src/test/java/com/hamster/sprite/dao/test/repository/PasswordAccountRepositoryTest.java
+++ hamster-sprite-dao/src/test/java/com/hamster/sprite/dao/test/repository/PasswordAccountRepositoryTest.java
@@ -9,13 +9,13 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.hamster.sprite.dao.test.base.AbstractDaoSpringTest;
+import com.hamster.sprite.dao.test.base.AbstractAppDaoSpringTest;
 
 /**
  * @author <a href="mailto:grossopaforever@gmail.com">Jack Yin</a>
  * @version 1.0
  */
-public class PasswordAccountRepositoryTest extends AbstractDaoSpringTest {
+public class PasswordAccountRepositoryTest extends AbstractAppDaoSpringTest {
 
     @Autowired
     private PasswordAccountRepository accountRepository;
